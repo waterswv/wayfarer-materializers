b5555e290f754e1233b12af63d96bb3ab72f4b7d
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-10 13:42:21 -0700

    updated css and folder strucutre. Fixed public ref in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "/public"));
 app.use(
   bodyParser.urlencoded({
     extended: true
